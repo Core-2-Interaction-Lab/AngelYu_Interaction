e504afe512b7b759b36be3cefb9c2d649cfda393
YuCandie <angelyu050@gmail.com>
2023-03-10 11:42:29 -0500

    fixing stuff

---
+++ Project2/assets/script.js
@@ -0,0 +1,13 @@
+const createCollectionHTML = (data) => {
+  const collectGrid = document.getElementById("collection");
+
+  data.forEach((item) => {
+    console.log(item);
+  });
+};
+
+fetch("collection.json")
+  .then((response) => response.json)
+  .then((data) => {
+    createCollectionHTML(data);
+  });
--- Week6_Wiki/assets/scripts.js
+++ Week6_Wiki/assets/scripts.js
@@ -1,48 +1,45 @@
 // Function to render your items
 const renderItems = (collection) => {
-    // The ul where the items will be inserted
-    const collectionList = document.getElementById('collection')
+  // The ul where the items will be inserted
+  const collectionList = document.getElementById("collection");
 
-    // Loop through each item in the collection array
-    collection.forEach(item => {
-        const listItem = document.createElement('li') // Make the li
+  // Loop through each item in the collection array
+  collection.forEach((item) => {
+    const listItem = document.createElement("li"); // Make the li
 
-        // You can make each element inside of that…
-        const itemTitle = document.createElement('h2') // Make an h2
-        itemTitle.innerHTML = item.title // Put the JSON title inside
-        listItem.appendChild(itemTitle) // And add it to the li!
+    // You can make each element inside of that…
+    const itemTitle = document.createElement("h2"); // Make an h2
+    itemTitle.innerHTML = item.title; // Put the JSON title inside
+    listItem.appendChild(itemTitle); // And add it to the li!
 
-        const itemImage = document.createElement('img') // And an image
-        itemImage.src = item.posterImage // Set the src attribute from the JSON
-        listItem.appendChild(itemImage) // And add that too
+    const itemImage = document.createElement("img"); // And an image
+    itemImage.src = item.posterImage; // Set the src attribute from the JSON
+    listItem.appendChild(itemImage); // And add that too
 
- 
-        // This can get annoying, so we can use “template literals” instead
-        const itemDetails =
-            `
+    // This can get annoying, so we can use “template literals” instead
+    const itemDetails = `
                 <p>📍 Found in <span>${item.location}</span></p>
                 <p>✍️ Found by ${item.partner}</p>
                 <details>
                     <summary>Read more</summary>
                     <p>${item.description}</p>
                 </details>
-            `
-        listItem.insertAdjacentHTML('beforeend', itemDetails) // Which can we then insert
+            `;
+    listItem.insertAdjacentHTML("beforeend", itemDetails); // Which can we then insert
 
-        // You can build logic from your data, too
-        // if (!item.otherAttr) { // If this is false
-        //     listItem.classList.add() // Add this class to the whole li
-        // }
+    // You can build logic from your data, too
+    // if (!item.otherAttr) { // If this is false
+    //     listItem.classList.add() // Add this class to the whole li
+    // }
 
-        collectionList.appendChild(listItem) // Then add the whole li into the ul
-    })
-}
-
+    collectionList.appendChild(listItem); // Then add the whole li into the ul
+  });
+};
 
 // Fetch gets your JSON file…
-fetch('assets/collection.json')
-    .then(response => response.json())
-    .then(collection => {
-        // And passes the data to the function, above!
-        renderItems(collection) // In reverse order
-})
+fetch("assets/collection.json")
+  .then((response) => response.json())
+  .then((collection) => {
+    // And passes the data to the function, above!
+    renderItems(collection); // In reverse order
+  });
