1a1af7381c8f71362759757e767156864230be70
YuCandie <angelyu050@gmail.com>
2023-05-09 02:02:45 -0400

    chart

---
+++ Project3/assets/script.js
@@ -0,0 +1,14 @@
+const createCollectionHTML = (data) => {
+    const collectGrid = document.getElementById("collection");
+  
+    data.forEach((item) => {
+      console.log(item);
+    });
+  };
+  
+  fetch("collection.json")
+    .then((response) => response.json)
+    .then((data) => {
+      createCollectionHTML(data);
+    });
+  ;
--- Project3/script.js
+++ Project3/script.js
@@ -51,3 +51,113 @@ function animateCount(obj, start, end, duration) {
 //Counting Up Deaths
 
 //Add AutoScroller button
+
+//Collection 
+		fetch("collection.json")
+    .then(response => response.json())
+    .then(data => {
+      const tableBody = document.querySelector("#data-table tbody");
+      const sortSelect = document.querySelector("#sort-by");
+      const sortOrderSelect = document.querySelector("#sort-order");
+
+      let sortedData = data.sort(compare("Year", "asc"));
+
+      updateTable();
+
+      sortSelect.addEventListener("change", updateTable);
+      sortOrderSelect.addEventListener("change", updateTable);
+
+      function updateTable() {
+        const sortBy = sortSelect.value;
+        const sortOrder = sortOrderSelect.value;
+
+        sortedData = data.sort(compare(sortBy, sortOrder));
+
+        tableBody.innerHTML = "";
+
+        sortedData.forEach(item => {
+          const row = tableBody.insertRow();
+
+          for (const key in item) {
+            const cell = row.insertCell();
+            cell.innerText = item[key];
+          }
+        });
+      }
+
+      function compare(prop, order) {
+        return function(a, b) {
+          if (order === "asc") {
+            if (a[prop] < b[prop]) {
+              return -1;
+            } else if (a[prop] > b[prop]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          } else {
+            if (a[prop] > b[prop]) {
+              return -1;
+            } else if (a[prop] < b[prop]) {
+              return 1;
+            } else {
+              return 0;
+            }
+          }
+        };
+      }
+    });
+
+    // Retrieve the JSON data
+fetch('collection.json')
+.then(response => response.json())
+.then(data => {
+  // Group the data by year and calculate the total number of deaths for each year
+  const groupedData = data.reduce((accumulator, currentValue) => {
+    const year = currentValue.Year;
+    const deaths = currentValue.Deaths;
+
+    if (!accumulator[year]) {
+      accumulator[year] = deaths;
+    } else {
+      accumulator[year] += deaths;
+    }
+
+    return accumulator;
+  }, {});
+
+  // Convert the grouped data into arrays for use in the chart
+  const years = Object.keys(groupedData);
+  const deaths = Object.values(groupedData);
+
+  // Create the chart using Chart.js
+  const ctx = document.getElementById('deaths-chart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: years,
+      datasets: [
+        {
+          label: 'Number of Deaths',
+          data: deaths,
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            }
+          }
+        ]
+      }
+    }
+  });
+})
+.catch(error => console.error(error));
